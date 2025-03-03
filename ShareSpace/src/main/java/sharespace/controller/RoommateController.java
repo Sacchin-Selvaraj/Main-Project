48906342221ea9dafad14ad739c8b53704caeaf1
Sacchin-Selvaraj <sacchin09082001@gmail.com>
2025-03-03 11:24:08 +0530

    Loggers

--- ShareSpace/src/main/java/sharespace/controller/RoommateController.java
+++ ShareSpace/src/main/java/sharespace/controller/RoommateController.java
@@ -1,5 +1,7 @@
 package sharespace.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import sharespace.model.*;
 import sharespace.payload.VacateResponseDTO;
@@ -18,64 +20,79 @@ public class RoommateController {
 
     private final RoommateService roommateService;
 
+    private static final Logger logger= LoggerFactory.getLogger(RoommateController.class);
+
     public RoommateController(RoommateService roommateService) {
         this.roommateService = roommateService;
     }
 
     @GetMapping("/all-roommates")
     public ResponseEntity<List<Roommate>> getAllRoommates(){
+        logger.info("Received an request to fetch all the Roommate details");
         List<Roommate> roommateList =roommateService.getAllRoommates();
+        logger.info("Fetched {} roommate details",roommateList.size());
         return new ResponseEntity<>(roommateList, HttpStatus.OK);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Roommate> updateRoommate(@PathVariable int id, @RequestBody @Valid Roommate roommate) {
+        logger.info("Received an request to update roommate details for roommate {}",id);
         Roommate updatedRoommate = roommateService.updateRoommate(id, roommate);
+        logger.info("Roommate details updated for the {}",updatedRoommate.getUsername());
         return new ResponseEntity<>(updatedRoommate, HttpStatus.OK);
     }
 
     @PatchMapping("/email/{id}")
     public ResponseEntity<Roommate> updateEmail(@PathVariable int id, @RequestBody @Valid EmailUpdateRequest emailUpdateRequest) {
         Roommate updatedRoommate = roommateService.updateEmail(id, emailUpdateRequest.getEmail());
+        logger.info("Email id was updated for the {}",updatedRoommate.getUsername());
         return new ResponseEntity<>(updatedRoommate, HttpStatus.OK);
     }
 
     @DeleteMapping("/vacate/{username}")
     public ResponseEntity<String> removeDetails(@PathVariable String username){
+        logger.info("Requested to vacate {} from the room",username);
         roommateService.deleteRoommate(username);
+        logger.info("{} have been removed Successfully",username);
         return new ResponseEntity<>("Roommate Detail have been removed Successfully",HttpStatus.OK);
 
     }
 
     @PostMapping("/get-roommate")
     public ResponseEntity<Roommate> getRoommate(@RequestBody LoginDetails loginDetails){
+        logger.info("{} is trying to login",loginDetails.getUsername());
         Roommate roommate= roommateService.getRoommate(loginDetails);
-
+        logger.info("{} was logged in successfully",loginDetails.getUsername());
         return new ResponseEntity<>(roommate,HttpStatus.OK);
     }
 
     @PatchMapping("/update-details/{roommateId}")
     public ResponseEntity<Roommate> updateDetails(@PathVariable int roommateId,@RequestBody UpdateDetails updateDetails){
+        logger.info("Received an request to update the roommate details for the roommateId {}",roommateId);
         Roommate roommate=roommateService.updateDetails(roommateId,updateDetails);
-
+        logger.info("Successfully updated details for the {}",roommate.getUsername());
         return new ResponseEntity<>(roommate,HttpStatus.OK);
     }
 
     @PostMapping("/send-vacate-request/{roommateId}")
     public ResponseEntity<String> sendVacateRequest(@PathVariable int roommateId, @RequestBody VacateRequest vacateRequest){
+        logger.info("Roommate {} have sent the vacate request",roommateId);
         String response=roommateService.sendVacateRequest(roommateId,vacateRequest);
+        logger.info("request have been submitted successfully");
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/pending-vacate-request")
     public ResponseEntity<List<VacateResponseDTO>> getPendingRequests() {
+        logger.info("Request for the pending payments list");
         List<VacateResponseDTO> vacateResponseDTOS = roommateService.getPendingVacateRequests();
-
+        logger.info("Still {} yet to pay the rent",vacateResponseDTOS.size());
         return ResponseEntity.ok(vacateResponseDTOS);
     }
 
     @PutMapping("/mark-read/{requestId}")
     public ResponseEntity<Void> markRequestAsRead(@PathVariable int requestId) {
+        logger.info("Received an request to mark the vacate request as read for {}",requestId);
         roommateService.markAsRead(requestId);
         return ResponseEntity.ok().build();
     }
@@ -89,7 +106,9 @@ public class RoommateController {
             @RequestParam(name = "sortOrder",defaultValue = "asc" ,required = false) String sortOrder
 
     ){
+        logger.info("Request to sort the Roommate details");
         Page<Roommate> roommates=roommateService.sortRoommates(page,limit,rentStatus,sortField,sortOrder);
+        logger.info("Received {} roommate details",roommates.getTotalElements());
         return new ResponseEntity<>(roommates,HttpStatus.OK);
     }
 
